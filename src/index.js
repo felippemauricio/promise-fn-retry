5ef803d88778e61d35fa1b186609ede7eaa87a0b
Felippe Maurício <felippemauriciov@gmail.com>
2018-11-19 23:12:21 -0200

    Add comments to code

--- src/index.js
+++ src/index.js
@@ -1,23 +1,41 @@
+/**
+ * Default options to options param on retry function
+ */
 const defaultOptions = {
-  times: 1,
-  initialDelayTime: 100,
-  onRetry: null,
-  shouldRetry: null,
+  times: 1, // Default retries
+  initialDelayTime: 100, // Initial delay to first retry (ms)
+  onRetry: null, // Function called in all retries. Is good to metrics, for example
+  shouldRetry: null, // Verify if is necessary to call retry. Is good to API error of auth.
 };
 
+/**
+ * Internal controls
+ */
 const controlOptions = {
-  retained: 0,
-  currentDelay: null,
+  retained: 0, // retries already made
+  currentDelay: null, // current delay that used in current retry
 };
 
+/**
+ * Build the options parsed object in order
+ */
 const buildOptionsParsed = options => ({
   ...defaultOptions,
   ...controlOptions,
   ...options,
 });
 
+/**
+ * Used to calc new delay on the next retry
+ * Today, this lib double the currentDelay
+ */
 const calcNewDelay = currentDelay => currentDelay * 2;
 
+/**
+ * Build the options to retry object
+ * This count the retry
+ * This calculate the new delay
+ */
 const buildOptionsToRetry = (optionsParsed) => {
   const { currentDelay, initialDelayTime, retained } = optionsParsed;
   return {
@@ -27,10 +45,20 @@ const buildOptionsToRetry = (optionsParsed) => {
   };
 };
 
+/**
+ * Verify if this lib should retry again
+ */
 const shouldRetryByExecutedTimes = ({ retained, times }) => retained < times;
 
+/**
+ * Use the setTimeout to delay retry
+ */
 const delay = delayTime => new Promise(resolve => setTimeout(resolve, delayTime));
 
+/**
+ * This run the lib
+ * Receive the promiseFn
+ */
 const retry = (requestFn, options = defaultOptions) => {
   const optionsParsed = buildOptionsParsed(options);
   const { onRetry, shouldRetry } = optionsParsed;
@@ -48,3 +76,4 @@ const retry = (requestFn, options = defaultOptions) => {
 
 
 export default retry;
+module.exports = retry;
