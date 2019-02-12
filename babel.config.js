5a09151f9816ab04138ae184a8236f144d2022b5
Felippe Maurício <felippemauriciov@gmail.com>
2019-02-12 21:13:49 -0200

    Add enviroment to config babel

--- babel.config.js
+++ babel.config.js
@@ -1,12 +1,16 @@
+const { NODE_ENV = 'development' } = process.env;
+const presetEnvOptions = NODE_ENV === 'production' ? {} : {
+  targets: {
+    node: true,
+  },
+};
+
+
 module.exports = {
   presets: [
     [
       '@babel/preset-env',
-      {
-        targets: {
-          node: true,
-        },
-      },
+      presetEnvOptions,
     ],
   ],
 };
